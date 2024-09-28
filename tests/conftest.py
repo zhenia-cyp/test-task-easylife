7c7df9afb5c7707fc8cc5f520ab177638d511f71
zhenia-cyp <kviat.zhenia@gmail.com>
2024-09-28 21:59:49 +0300

    test: created a pytest for the get_all_users() method in the UserService

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,13 @@
 import pytest
 from unittest.mock import AsyncMock
 from sqlalchemy.ext.asyncio import AsyncSession
-
 from app.services.transaction_service import TransactionService
 from app.services.user_service import UserService
 
-
+"""
+https://pytest-with-eric.com/pytest-advanced/pytest-asyncio/
+   8. Async Mocking Using AsyncMock
+"""
 @pytest.fixture(scope="function")
 def mock_session():
     return AsyncMock(spec=AsyncSession)
--- tests/test_user_service.py
+++ tests/test_user_service.py
@@ -1,6 +1,7 @@
 import pytest
 from unittest.mock import AsyncMock
 from app.models.model import User, Transaction
+from app.schemas.pagination import PaginationListResponse, PageParams
 from app.schemas.schema import UserCreate, UserResponse, UserTransactionsResponse
 
 
@@ -37,3 +38,45 @@ async def test_get_user(user_service):
     assert len(result.transactions) == 2
     assert result.transactions[0].amount == 100
     assert result.transactions[1].amount == 200
+
+
+@pytest.mark.asyncio
+async def test_get_all_users(user_service):
+    mock_user = User(id=1, username="user1")
+    mock_transactions_user = Transaction(id=1, user_id=1, transaction_type="credit", amount=100.0, transaction_date="2024-01-01")
+
+    user_transactions_response = [
+        UserTransactionsResponse(
+            user_id=mock_user.id,
+            username=mock_user.username,
+            transactions=[
+                {
+                    "id": mock_transactions_user.id,
+                    "transaction_type": mock_transactions_user.transaction_type,
+                    "amount": mock_transactions_user.amount,
+                    "transaction_date": mock_transactions_user.transaction_date
+                }
+            ]
+        )
+    ]
+    user_service.get_all_users = AsyncMock(return_value=PaginationListResponse(
+        current_page=1,
+        size=10,
+        total_items=1,
+        total_pages=1,
+        result=user_transactions_response
+    ))
+    page_params = PageParams(page=1, size=10)
+    result = await user_service.get_all_users(page_params)
+    print(result.result[0].transactions)
+    assert isinstance(result, PaginationListResponse)
+    assert result.current_page == 1
+    assert result.size == 10
+    assert result.total_items == 1
+    assert len(result.result) == 1
+
+    assert result.result[0].user_id == mock_user.id
+    assert result.result[0].username == mock_user.username
+    assert len(result.result[0].transactions) == 1
+    assert result.result[0].transactions[0].transaction_type == "credit"
+    assert result.result[0].transactions[0].amount == 100.0
